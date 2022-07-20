a23bf2ac87ac252694c81db05f062b245eaa6faf
ibnshayed <ibnshayed@gmail.com>
2022-07-20 15:05:00 +0600

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,105 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [todoText, setTodoText] = useState("");
+  const [todoTextList, setTodoTextList] = useState([]);
+
+  const addTodoHandler = () => {
+    setTodoTextList((currentTodoTextList) => [
+      ...currentTodoTextList,
+      todoText,
+    ]);
+    setTodoText("");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.root}>
+      <View style={styles.todoForm}>
+        <TextInput
+          style={styles.todoTextInput}
+          placeholder="Enter a todo"
+          onChangeText={setTodoText}
+          value={todoText}
+        />
+        <View style={styles.submitButton}>
+          <Button title="submit" onPress={addTodoHandler} />
+        </View>
+      </View>
+        <View style={styles.clearButtonRoot}>
+          <Button style={styles.clearButton} title="Clear" onPress={() => setTodoTextList([])} />
+        </View>
+      <View style={styles.todoListRoot}>
+        {todoTextList.length > 0 && (
+          <Text style={{ fontSize: 24 }}>Todo List...</Text>
+        )}
+        {/* {todoTextList &&
+          todoTextList.map((todoText) => (
+            <Text key={Math.random()} style={styles.todoText}>{todoText}</Text>
+          ))} */}
+				 
+        <FlatList
+          data={todoTextList}
+          renderItem={({ item, index, separators }) => (
+            <Text key={index} style={styles.todoText}>
+              {item}
+            </Text>
+          )}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  root: {
+    backgroundColor: "white",
+    padding: 20,
+    height: "100%",
   },
+  todoForm: {
+    marginTop: 20,
+    flexDirection: "row",
+  },
+  todoTextInput: {
+    width: "70%",
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderLeftWidth: 1,
+    paddingLeft: 10,
+  },
+  submitButton: {
+    width: "30%",
+  },
+  todoListRoot: {
+    marginTop: 40,
+  },
+  todoText: {
+    backgroundColor: "blue",
+    color: "white",
+    padding: 10,
+    marginVertical: 3,
+    borderRadius: 10,
+	},
+	clearButtonRoot: {
+		position: "absolute",
+		right: 50,
+		bottom: 100,
+		borderRadius: 50,
+		height: 150,
+		width: 150,
+		overflow: 'hidden',
+	},
+	clearButton: {
+		backgroundColor: "green",
+		padding: 10,
+	}
 });
